96f0ce01dbb0703660d93b7d19ce078c6852ffbf
xHire <xhire@tuxportal.cz>
2008-10-27 23:22:01 +0100

    Script for automatic code indentation to K&R

---
+++ kr.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! "$1" ]; then
+	echo "Help:  this script indents code according to the K&R standard"
+	echo "Usage: $0 path/to/file.c"
+	exit 1
+elif [ ! -e "$1" ]; then
+	echo "Error: file '$1' doesn't exist!"
+	exit 1
+fi
+
+indent -kr -l79 -ts4 -ip4 -ppi 4 "$1"
