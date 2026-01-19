fa211ea82c8589785372771daaebc425f24710b5
Connor Thomson <blumatrikz@gmail.com>
2026-01-18 17:22:21 -0800

    Update appimage.sh

--- packaging/appimage/appimage.sh
+++ packaging/appimage/appimage.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
+
+# Exit on error
 set -e
+
+# Tuxánci version
 VERSION="0.22.0"
-echo "Building Tuxánci appimage..."
+
+# Make build directory
 mkdir -p ../../build tuxanci-$VERSION.AppDir
+
+# Configure
 cmake -S ../../ -B ../../build
+
+# Compile
 make -j$(( $(nproc) + 1 )) -C ../../build
+
+# Install
 make DESTDIR=../packaging/appimage/tuxanci-$VERSION.AppDir -j$(( $(nproc) + 1 )) -C ../../build install
-rm -f tuxanci-$VERSION.AppDir/tuxanci.desktop tuxanci-$VERSION.AppDir/tuxanci.svg
-ln -s usr/share/applications/tuxanci.desktop tuxanci-$VERSION.AppDir/tuxanci.desktop
+
+# Remove build artifacts
+rm -f tuxanci-$VERSION.AppDir/tuxanci.desktop tuxanci-$VERSION.AppDir/tuxanci.svg tuxanci-$VERSION-x86_64.AppImage*
+
+# Symlink desktop file
+ln -s usr/share/applications/tuxanci.desktop tuxanci-$VERSION.AppDir/tuxanci-$VERSION.desktop
+
+# Symlink icon
 ln -s usr/local/share/pixmaps/tuxanci.svg tuxanci-$VERSION.AppDir/tuxanci.svg
+
+# Copy AppDir
 cp templates/AppRun tuxanci-$VERSION.AppDir
-rm -f tuxanci-$VERSION-x86_64.AppImage*
+
+# Download appimagetool
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+
+# Make appimagetool executable
 chmod +x appimagetool-x86_64.AppImage
+
+# Run appimagetool
 ./appimagetool-x86_64.AppImage ./tuxanci-$VERSION.AppDir
+
+# Remove build artifacts
 rm -rf appimagetool-x86_64.AppImage* tuxanci-$VERSION.AppDir
